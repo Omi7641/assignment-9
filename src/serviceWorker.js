3c2905dd137c5c32ed44468fd0c61e840bfbbc11
Mehedi Hasan <onlinehasan95@gmail.com>
2020-09-25 19:35:49 +0600

    all worked are done

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -5,20 +5,16 @@ const isLocalhost = Boolean(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
-
 export function register(config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       return;
     }
-
     window.addEventListener("load", () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
-
         navigator.serviceWorker.ready.then(() => {
           console.log(
             "This web app is being served cache-first by a service " +
@@ -31,7 +27,6 @@ export function register(config) {
     });
   }
 }
-
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
@@ -48,13 +43,11 @@ function registerValidSW(swUrl, config) {
                 "New content is available and will be used when all " +
                   "tabs for this page are closed. See https://bit.ly/CRA-PWA."
               );
-
               if (config && config.onUpdate) {
                 config.onUpdate(registration);
               }
             } else {
               console.log("Content is cached for offline use.");
-
               if (config && config.onSuccess) {
                 config.onSuccess(registration);
               }
@@ -67,7 +60,6 @@ function registerValidSW(swUrl, config) {
       console.error("Error during service worker registration:", error);
     });
 }
-
 function checkValidServiceWorker(swUrl, config) {
   fetch(swUrl, {
     headers: { "Service-Worker": "script" },
@@ -93,7 +85,6 @@ function checkValidServiceWorker(swUrl, config) {
       );
     });
 }
-
 export function unregister() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.ready
